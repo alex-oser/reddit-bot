18337312b9b1ea2e459734a0f2fc7e7094101275
Alex  Oser <alex@Macintosh.local>
2019-03-14 21:40:35 -0500

    Adding word processing

--- reddit_scraper.sql
+++ reddit_scraper.sql
@@ -1,18 +1,26 @@
 -- sqlite3 reddit_scraper.db ".read reddit_scraper.sql"
 -- sqlite3 reddit_scraper.db "select * from words"
--- sqlite3 reddit_scraper.db "select * from posts;select * from words;"
 
-DROP TABLE IF EXISTS posts;
-CREATE TABLE posts (
-post_id VARCHAR(10),
-created_time TIME,
-title VARCHAR(300),
-url VARCHAR(300)
+DROP TABLE IF EXISTS submissions;
+CREATE TABLE submissions (
+	submission_id VARCHAR(10),
+	created_time INT,
+	title VARCHAR(300),
+	url VARCHAR(300)
 );
 
 DROP TABLE IF EXISTS words;
 CREATE TABLE words (
-word VARCHAR(100),
-count INT DEFAULT 1,
-PRIMARY KEY (word)
+	original_word VARCHAR(100),
+	processed_word VARCHAR(100),
+	submission_id VARCHAR(10),
+	timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY(submission_id) REFERENCES submissions(submission_id)
 );
+
+DROP VIEW IF EXISTS proc_sum;
+CREATE VIEW proc_sum AS
+	SELECT processed_word, count(*) as count
+	FROM words
+	GROUP BY processed_word
+	ORDER BY count DESC;
--- scraper.py
+++ scraper.py
@@ -1,59 +1,67 @@
 #!/usr/bin/env python
 
 import praw
+import re
 import sqlite3
 from sqlite3 import Error
 
-def get_posts(sub):
+def get_submissions(sub):
     """ create a connection to the Reddit API and pull data
-    :param sub: subreddit to pull posts from
-    :return: dictionary containing post id as the key and the post as the value
+    :param sub: subreddit to pull submissions from
+    :return: dictionary containing submission id as the key and the submission as the value
     """
     reddit = praw.Reddit('random_bot')
-    posts={}
-    for post in reddit.subreddit(sub).new(limit=10):
-        submission = reddit.submission(id="{}".format(post))
+    submissions={}
+    for submission in reddit.subreddit(sub).new():
         title = submission.title
-        posts[submission.id]=submission
-    return posts
+        submissions[submission.id]=submission
+    return submissions
 
-def check_post(conn, post_id, post):
-    """ checks to see if this post has already been parsed or not
-    :param post_id: id of the post to check
-    :return: true if the post has not been parsed yet, false otherwise 
+def check_submission(conn, submission):
+    """ checks to see if this submission has already been parsed or not
+    :param submission_id: id of the submission to check
+    :return: true if the submission has not been parsed yet, false otherwise 
     """
     cur = conn.cursor()
-    sql = ''' SELECT COUNT(*) FROM posts WHERE post_id = ? '''
-    cur.execute(sql, (post_id,))     
-    numRows = cur.fetchone()[0]
-    if numRows < 1:
-        print("Found a new post")
-        sql = '''INSERT INTO posts (post_id, created_time, title, url) \
+    sql = ''' SELECT COUNT(*) FROM submissions WHERE submission_id = ? '''
+    cur.execute(sql, (submission.id,))     
+    count = cur.fetchone()[0]
+    if count < 1:
+        print("Found a new submission")
+        sql = '''INSERT INTO submissions (submission_id, created_time, title, url) \
             VALUES (?,?,?,?)'''
-        cur.execute(sql, (post_id,post.created_utc,post.title,post.url))
+        cur.execute(sql, (submission.id,submission.created_utc,submission.title,submission.url))
         return True
     else:
         return False
 
-def insert_post(conn, words):
-    """ performs an insert or update to keep track of the count of words used in post titles """
+def insert_submission(conn, submission):
+    """ performs an insert or update to keep track of the count of words used in submission titles """
     cur = conn.cursor()
-    for word in words:
-        sql = ''' INSERT OR REPLACE INTO words (word, count) \
-            VALUES (?, COALESCE((SELECT count FROM words WHERE word=?)+1,1)) '''
-        cur.execute(sql, (word,word))
+    words=submission.title.split()
+    processed_words=process_words(words)
+    for original_word, processed_word in zip(words,processed_words):
+        if processed_word != "":
+            sql = ''' INSERT OR REPLACE INTO words (original_word, processed_word, submission_id) \
+                VALUES (?,?,?) '''
+            cur.execute(sql, (original_word,processed_word,submission.id))
 
-def record_posts(subreddit):
-    """ inserts data into the posts and words tables """
-    print("Getting posts from r/{}...".format(subreddit))
-    posts=get_posts(subreddit)
+def process_words(words):
+    """ performs text processing on words in a submission to standardize them for further analysis """
+    processed_words = map(lambda x: x.lower(), words)
+    processed_words = map(lambda x: re.sub('[^0-9a-zA-Z]+', '', x), processed_words)
+    return processed_words
+
+def record_submissions(subreddit):
+    """ inserts data into the submissions and words tables """
+    print("Getting submissions from r/{}...".format(subreddit))
+    submissions=get_submissions(subreddit)
     conn=db_connection(database)
     with conn:
-        print("Inserting new content into posts and words tables...")
-        for post_id, post in posts.items():
-            if check_post(conn, post_id, post):
-                words=post.title.split()
-                insert_post(conn, words)
+        print("Inserting new content into submissions and words tables...")
+        for submission in submissions.values():
+            if check_submission(conn, submission):
+                insert_submission(conn, submission)
         print("Committing changes and closing the connection.")
         conn.commit()
 
@@ -72,9 +80,9 @@ def db_connection(db_file):
     return None
     
 def main():
-    record_posts('all')
+    record_submissions('stocks')
   
-database="/Users/alex/Coding/praw/reddit_scraper.db"
+database="/Users/alex/Coding/reddit-bot/reddit_scraper.db"
 if __name__== "__main__":
   main()
 
