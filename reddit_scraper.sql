81361a94693f9bf9d36f9962a993a747e65ba23f
Alex  Oser <alex@Macintosh.local>
2019-03-15 23:42:42 -0500

    Adding functionality to get words from comments as well as titles

--- reddit_scraper.sql
+++ reddit_scraper.sql
@@ -1,11 +1,12 @@
 -- sqlite3 reddit_scraper.db ".read reddit_scraper.sql"
 -- sqlite3 reddit_scraper.db "select * from words"
 
-DROP TABLE IF EXISTS submissions;
-CREATE TABLE submissions (
-	submission_id VARCHAR(10),
+DROP TABLE IF EXISTS objects;
+CREATE TABLE objects (
+	id VARCHAR(10),
+	type VARCHAR(10),
 	created_time INT,
-	title VARCHAR(300),
+	content VARCHAR(300),
 	url VARCHAR(300)
 );
 
@@ -13,9 +14,9 @@ DROP TABLE IF EXISTS words;
 CREATE TABLE words (
 	original_word VARCHAR(100),
 	processed_word VARCHAR(100),
-	submission_id VARCHAR(10),
+	object_id VARCHAR(10),
 	timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY(submission_id) REFERENCES submissions(submission_id)
+	FOREIGN KEY(object_id) REFERENCES objects(object_id)
 );
 
 DROP VIEW IF EXISTS proc_sum;
--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import praw
 import re
 import sqlite3
 from sqlite3 import Error
+import sys
 
 def get_submissions(sub):
     """ create a connection to the Reddit API and pull data
@@ -12,45 +13,75 @@ def get_submissions(sub):
     """
     reddit = praw.Reddit('random_bot')
     submissions={}
-    for submission in reddit.subreddit(sub).new():
+    for submission in reddit.subreddit(sub).new(limit=10):
         title = submission.title
         submissions[submission.id]=submission
     return submissions
 
-def check_submission(conn, submission):
-    """ checks to see if this submission has already been parsed or not
-    :param submission_id: id of the submission to check
-    :return: true if the submission has not been parsed yet, false otherwise 
+def check_object(conn, praw_object):
+    """ checks to see if this object has already been parsed or not
+    :param submission_id: id of the object to check
+    :return: true if the object has not been parsed yet, false otherwise 
     """
     cur = conn.cursor()
-    sql = ''' SELECT COUNT(*) FROM submissions WHERE submission_id = ? '''
-    cur.execute(sql, (submission.id,))     
+    sql = ''' SELECT COUNT(*) FROM objects WHERE id = ? '''
+    cur.execute(sql, (praw_object.fullname,))     
     count = cur.fetchone()[0]
     if count < 1:
-        print("Found a new submission")
-        sql = '''INSERT INTO submissions (submission_id, created_time, title, url) \
-            VALUES (?,?,?,?)'''
-        cur.execute(sql, (submission.id,submission.created_utc,submission.title,submission.url))
+        print("Found a new object")
+        praw_object_type = get_object_type(praw_object.fullname)
+        sql = '''INSERT INTO objects (id, type, created_time, content, url) \
+            VALUES (?,?,?,?,?)'''
+        values=get_object_values(praw_object, praw_object_type)
+        cur.execute(sql, values)
         return True
     else:
         return False
 
-def insert_submission(conn, submission):
-    """ performs an insert or update to keep track of the count of words used in submission titles """
-    cur = conn.cursor()
-    words=submission.title.split()
-    processed_words=process_words(words)
-    for original_word, processed_word in zip(words,processed_words):
-        if processed_word != "":
-            sql = ''' INSERT OR REPLACE INTO words (original_word, processed_word, submission_id) \
-                VALUES (?,?,?) '''
-            cur.execute(sql, (original_word,processed_word,submission.id))
+def get_object_values(praw_object, praw_object_type):
+    """ insert object information to the objects table """
+    if praw_object_type == "Comment":
+        values = (praw_object.fullname,praw_object_type,praw_object.created_utc,praw_object.body,praw_object.permalink)
+    elif praw_object_type == "Link":
+        values = (praw_object.fullname,praw_object_type,praw_object.created_utc,praw_object.title,praw_object.url)
+    return values
+    
+
+def get_object_type(object_id):
+    """returns the type of object per https://www.reddit.com/dev/api/"""
+    object_types = {
+        "t1": "Comment",
+        "t2": "Account",
+        "t3": "Link",
+        "t4": "Message",
+        "t5": "Subreddit",
+        "t6": "Award"
+    }
+    return object_types[object_id[:2]]
+
+def process_submission(conn, submission):
+    """ get the words from each object in the submission and insert them """
+    words = submission.title.split()
+    insert_words(conn, words, submission.fullname)
+    for comment in submission.comments:
+        words = comment.body.split()
+        insert_words(conn, words, comment.fullname)
 
 def process_words(words):
     """ performs text processing on words in a submission to standardize them for further analysis """
     processed_words = map(lambda x: x.lower(), words)
     processed_words = map(lambda x: re.sub('[^0-9a-zA-Z]+', '', x), processed_words)
     return processed_words
+
+def insert_words(conn, words, object_id):
+    """ performs an insert or update to keep track of the count of words used in submission titles """
+    cur = conn.cursor()
+    processed_words=process_words(words)
+    for original_word, processed_word in zip(words,processed_words):
+        if processed_word != "":
+            sql = ''' INSERT INTO words (original_word, processed_word, object_id) \
+                VALUES (?,?,?) '''
+            cur.execute(sql, (original_word,processed_word,object_id))
 
 def record_submissions(subreddit):
     """ inserts data into the submissions and words tables """
@@ -60,8 +91,8 @@ def record_submissions(subreddit):
     with conn:
         print("Inserting new content into submissions and words tables...")
         for submission in submissions.values():
-            if check_submission(conn, submission):
-                insert_submission(conn, submission)
+            if check_object(conn, submission):
+                process_submission(conn, submission)
         print("Committing changes and closing the connection.")
         conn.commit()
 
